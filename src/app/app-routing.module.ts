3258726225f191d24ecbd47a1354360986b137a0
Matea Gjika <mateagjika@Mateas-MacBook-Pro.local>
2021-08-18 15:22:21 +0200

    New changes, nothing substantial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-{ path: '', redirectTo:'', pathMatch:'full'},
-{ path: 'habits', loadChildren: () => import('./habits/habits.module').then(m => m.HabitsModule)}
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+{ 
+    path: 'habits', loadChildren: () => import('./habits/habits.module').then(m => m.HabitsModule)}
 ];
 
 @NgModule({
--- src/app/habits/habits-routing.module.ts
+++ src/app/habits/habits-routing.module.ts
@@ -5,7 +5,7 @@ import { HabitsComponent } from './habits.component';
 
 const routes: Routes = [
   {
-   path: '', component: HabitsComponent }
+   path: ':id', component: HabitsComponent }
   ];
 
 @NgModule({
